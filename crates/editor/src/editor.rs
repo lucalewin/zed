fee0f3baa81534aea203b2d5f3209ee97b5a6f80
Kirill Bulatov <kirill@zed.dev>
2025-07-17 13:11:23 +0300

    Map the new interface
    # Conflicts:
    #	crates/project/src/lsp_store.rs
    
    # Conflicts:
    #	crates/project/src/lsp_store.rs
    
    # Conflicts:
    #	crates/editor/src/editor.rs
    #	crates/project/src/lsp_store.rs
    
    # Conflicts:
    #	crates/project/src/lsp_store.rs

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -1619,7 +1619,7 @@ enum InlayHintRefreshReason {
     SettingsChange(InlayHintSettings),
     NewLinesShown,
     BufferEdited(HashSet<Arc<Language>>),
-    RefreshRequested,
+    RefreshRequested(LanguageServerId),
     ExcerptsRemoved(Vec<ExcerptId>),
 }
 
@@ -1631,7 +1631,7 @@ impl InlayHintRefreshReason {
             Self::SettingsChange(_) => "settings change",
             Self::NewLinesShown => "new lines shown",
             Self::BufferEdited(_) => "buffer edited",
-            Self::RefreshRequested => "refresh requested",
+            Self::RefreshRequested(_) => "refresh requested",
             Self::ExcerptsRemoved(_) => "excerpts removed",
         }
     }
@@ -1872,8 +1872,11 @@ impl Editor {
                     project::Event::RefreshCodeLens => {
                         // we always query lens with actions, without storing them, always refreshing them
                     }
-                    project::Event::RefreshInlayHints => {
-                        editor.refresh_inlay_hints(InlayHintRefreshReason::RefreshRequested, cx);
+                    project::Event::RefreshInlayHints(server_id) => {
+                        editor.refresh_inlay_hints(
+                                InlayHintRefreshReason::RefreshRequested(*server_id),
+                                cx,
+                            );
                     }
                     project::Event::LanguageServerAdded(..)
                     | project::Event::LanguageServerRemoved(..) => {
@@ -5270,7 +5273,7 @@ impl Editor {
             InlayHintRefreshReason::BufferEdited(buffer_languages) => {
                 (InvalidationStrategy::BufferEdited, Some(buffer_languages))
             }
-            InlayHintRefreshReason::RefreshRequested => {
+            InlayHintRefreshReason::RefreshRequested(_) => {
                 (InvalidationStrategy::RefreshRequested, None)
             }
         };
@@ -17341,9 +17344,9 @@ impl Editor {
                         HashSet::default(),
                         cx,
                     );
-                    cx.emit(project::Event::RefreshInlayHints);
                 });
             });
+            self.refresh_inlay_hints(InlayHintRefreshReason::NewLinesShown, cx);
         }
     }
 
